ee7af661222d576f79dd6fbcad2931cd85e1898e
Taurus Colvin <taurus.colvin@gmail.com>
2018-10-24 04:23:45 -0400

    map fixed-width address fields

---
+++ src/field-map.js
@@ -0,0 +1,64 @@
+const fieldMap = [
+  {
+		name: "houseNumber",
+		width: 30,
+		start: 1,
+		type: "string"
+  },
+  {
+		name: "streetDirectionPrefix",
+		width: 2,
+		start: 31,
+		type: "string"
+  },
+  {
+		name: "streetName",
+		width: 40,
+		start: 33,
+		type: "string"
+  },
+  {
+		name: "streetSuffix",
+		width: 4,
+		start: 73,
+		type: "string"
+  },
+  {
+		name: "streetDirectionSuffix",
+		width: 2,
+		start: 77,
+		type: "string"
+  },
+  {
+		name: "unitDescriptor",
+		width: 10,
+		start: 79,
+		type: "string"
+  },
+  {
+		name: "unitNumber",
+		width: 6,
+		start: 89,
+		type: "string"
+  },
+  {
+		name: "city",
+		width: 30,
+		start: 95,
+		type: "string"
+  },
+  {
+		name: "state",
+		width: 2,
+		start: 125,
+		type: "string"
+  },
+  {
+		name: "zip",
+		width: 12,
+		start: 127,
+		type: "string"
+  },
+]
+
+export default fieldMap
